c410587c02cd846d55526fa0dc2ce97c527a26dc
Admin <iheachochristian@yahoo.com>
2023-01-19 10:49:10 +0000

    Iteration to map through and construct tree

--- build-tree.js
+++ build-tree.js
@@ -15,7 +15,19 @@ const buildTree = nodes => {
     const nodeMap = new Map()
     nodes.forEach(node => nodeMap.set(node.nodeId, node))
 
-
+    //This performs the iteration thhrough the map and constructs thhe tree
     const rootNodes = []
+    nodeMap.forEach((node) => {
+        if (node.parentId) {
+            const parent = nodeMap.get(node.parentId);
+            if (!parent.children) {
+                parent.children = [];
+            }
+            insertNodeInParent(node, parent);
+        } else {
+            rootNodes.push(node);
+        }
+    });
 
+    return rootNodes;
 }
